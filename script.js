c43cbebabb625c267a27b1346fbf57be98732807
Abhinav Choubey <98657817+abhinavchoubey2000@users.noreply.github.com>
2022-05-19 18:23:35 +0530

    Updated Button Logic

--- script.js
+++ script.js
@@ -2,19 +2,28 @@ function showAvatar() {
     const name = document.getElementById("name");
     const avatar = document.getElementById("avatar");
     const imageBox = document.getElementById("image-box");
+    const cancel = document.getElementById("cancel-image");
+
     if(name.value==""){
         setTimeout(() => {
-            imageBox.innerHTML = `<img class="box-circle my-30" id="avatar" src="./default_avatar.png" alt="">`;    
+            imageBox.innerHTML = `<img class="box-circle my-30" id="avatar" src="./avatar.png" alt="">`;    
         }, 5000);
         imageBox.innerHTML = `<h2>The Name Field Is Required! 😒</h2>`;
     }
     else if(name.value.split(" ").length==2){
         setTimeout(() => {
-            imageBox.innerHTML = `<img class="box-circle my-30" id="avatar" src="./default_avatar.png" alt="">`;    
+            imageBox.innerHTML = `<img class="box-circle my-30" id="avatar" src="./avatar.png" alt="">`;    
         }, 5000);
         imageBox.innerHTML = `<h2>I Said Enter Your First Name Only! 😒</h2>`;
     }
     else if(name.value!=""){
+        cancel.style.display = "block";
+
+        cancel.addEventListener("click",()=>{
+            name.value = "";
+            cancel.style.display="none";
+            imageBox.innerHTML = `<img class="box-circle my-30" id="avatar" src="./avatar.png" alt="">`; 
+        })
         const url = `https://joeschmoe.io/api/v1/${name.value}`;
         avatar.src = url;
         setTimeout(() => {
